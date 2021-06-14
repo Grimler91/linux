9856c8c5789453589bb51f113128480c53f571d9
Henrik Grimler <henrik@grimler.se>
2021-06-14 17:06:31 +0200

    panel-samsung-ea8061v-ams497ee01: use current brightness in init
    And update code to match s6e3fa2 more closely.

--- drivers/gpu/drm/panel/panel-samsung-ea8061v-ams497ee01.c
+++ drivers/gpu/drm/panel/panel-samsung-ea8061v-ams497ee01.c
@@ -2,6 +2,9 @@
 // Copyright (c) 2021 FIXME
 // Generated with linux-mdss-dsi-panel-driver-generator from vendor device tree:
 //   Copyright (c) 2013, The Linux Foundation. All rights reserved. (FIXME)
+//
+// Mostly based on s6e3fa2 and s6e63m0. Table values taken from Samsung's
+// android kernel
 
 #include <drm/drm_mipi_dsi.h>
 #include <drm/drm_modes.h>
@@ -374,6 +377,7 @@ struct ea8061v_ams497ee01 {
 	struct gpio_desc *reset_gpio;
 
 	bool prepared;
+	bool enabled;
 };
 
 static inline
@@ -389,78 +393,6 @@ struct ea8061v_ams497ee01 *to_ea8061v_ams497ee01(struct drm_panel *panel)
 		if (ret < 0)						\
 			return ret;					\
 	} while (0)
-
-static void ea8061v_ams497ee01_reset(struct ea8061v_ams497ee01 *ctx)
-{
-	gpiod_set_value_cansleep(ctx->reset_gpio, 0);
-	msleep(20);
-	gpiod_set_value_cansleep(ctx->reset_gpio, 1);
-	usleep_range(1000, 2000);
-	gpiod_set_value_cansleep(ctx->reset_gpio, 0);
-	msleep(20);
-}
-
-static int ea8061v_ams497ee01_on(struct ea8061v_ams497ee01 *ctx)
-{
-	struct mipi_dsi_device *dsi = ctx->dsi;
-	struct device *dev = &dsi->dev;
-	int ret;
-
-	dsi->mode_flags |= MIPI_DSI_MODE_LPM;
-
-	/* unlock, f0 */
-	dsi_dcs_write_seq(dsi, 0xf0, 0x5a, 0x5a);
-	/* unlock, f1*/
-	dsi_dcs_write_seq(dsi, 0xf1, 0x5a, 0x5a);
-
-	/* Common conditions */
-	/* set (ref?) temperature/voltage (0x19 = 25 degC, 0x10 = 7.6 V) */
-	dsi_dcs_write_seq(dsi, 0xb8, 0x19, 0x10);
-	/* ? */
-	dsi_dcs_write_seq(dsi, 0xba, 0x57);
-	/* unlock, fc */
-	dsi_dcs_write_seq(dsi, 0xfc, 0x5a, 0x5a);
-	/* skip 11 parameters in HBM sequence ? */
-	dsi_dcs_write_seq(dsi, 0xb0, 0x0b);
-	/* ? */
-	dsi_dcs_write_seq(dsi, 0xd2, 0x00, 0x85);
-	/* ? */
-	dsi_dcs_write_seq(dsi, 0xcb, 0x70);
-	/* lock, fc */
-	dsi_dcs_write_seq(dsi, 0xfc, 0xa5, 0xa5);
-
-	/* Brightness control */
-	/* update gamma */
-	dsi_dcs_write_seq(dsi, 0xca,
-			  0x01, 0x00, 0x01, 0x00, 0x01, 0x00, 0x80, 0x80, 0x80,
-			  0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80,
-			  0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80,
-			  0x80, 0x80, 0x80, 0x00, 0x00, 0x00);
-	/* aid/aor */
-	dsi_dcs_write_seq(dsi, 0xb2, 0x00, 0x00, 0x00, 0x0a);
-	/* elvss (with acl off) */
-	dsi_dcs_write_seq(dsi, 0xb6, 0x5c, 0x8a);
-	/* gamma update */
-	dsi_dcs_write_seq(dsi, 0xf7, 0x01);
-
-	ret = mipi_dsi_dcs_exit_sleep_mode(dsi);
-	if (ret < 0) {
-		dev_err(dev, "Failed to exit sleep mode: %d\n", ret);
-		return ret;
-	}
-	msleep(120);
-
-	/* lock, f1 */
-	dsi_dcs_write_seq(dsi, 0xf1, 0xa5, 0xa5);
-
-	ret = mipi_dsi_dcs_set_display_on(dsi);
-	if (ret < 0) {
-		dev_err(dev, "Failed to set display on: %d\n", ret);
-		return ret;
-	}
-
-	return 0;
-}
 
 static int ea8061v_ams497ee01_get_brightness(struct backlight_device *bl_dev)
 {
@@ -551,21 +483,15 @@ static int ea8061v_ams497ee01_set_brightness(struct backlight_device *bl_dev)
 
 	/* aid/aor */
 	ea8061v_ams497ee01_update_aid(ctx, brightness);
-
 	/* acl opr on */
 	dsi_dcs_write_seq(dsi, 0xb5, 0x29);
 	/* acl off (0x55, 0x02 for 15 % ACL) */
 	dsi_dcs_write_seq(dsi, 0x55, 0x00);
-	ea8061v_ams497ee01_gamma_update(ctx);
-
 	/* elvss b6 */
 	ea8061v_ams497ee01_update_elvss(ctx, brightness);
 
-	/* Temperature corrections would go here */
-
 	/* gamma ca */
 	ea8061v_ams497ee01_update_gamma(ctx, brightness);
-
 	ea8061v_ams497ee01_gamma_update(ctx);
 
 	// /* lock f0 */
@@ -581,28 +507,158 @@ static const struct backlight_ops ea8061v_ams497ee01_bl_ops = {
 	.update_status = ea8061v_ams497ee01_set_brightness,
 };
 
-static int ea8061v_ams497ee01_off(struct ea8061v_ams497ee01 *ctx)
+static int ea8061v_ams497ee01_init(struct ea8061v_ams497ee01 *ctx)
 {
 	struct mipi_dsi_device *dsi = ctx->dsi;
 	struct device *dev = &dsi->dev;
 	int ret;
 
+	unsigned int brightness = ctx->bl_dev->props.brightness;
+
+	dsi->mode_flags |= MIPI_DSI_MODE_LPM;
+
+	/* unlock, f0 */
+	dsi_dcs_write_seq(dsi, 0xf0, 0x5a, 0x5a);
+	/* unlock, f1 */
+	dsi_dcs_write_seq(dsi, 0xf1, 0x5a, 0x5a);
+
+	/* Common conditions */
+	/* set (ref?) temperature/voltage (0x19 = 25 degC, 0x10 = 7.6 V) */
+	dsi_dcs_write_seq(dsi, 0xb8, 0x19, 0x10);
+	/* ? */
+	dsi_dcs_write_seq(dsi, 0xba, 0x57);
+	/* unlock, fc */
+	dsi_dcs_write_seq(dsi, 0xfc, 0x5a, 0x5a);
+	/* skip 11 parameters in HBM sequence ? */
+	dsi_dcs_write_seq(dsi, 0xb0, 0x0b);
+	/* ? */
+	dsi_dcs_write_seq(dsi, 0xd2, 0x00, 0x85);
+	/* ? */
+	dsi_dcs_write_seq(dsi, 0xcb, 0x70);
+	/* lock, fc */
+	dsi_dcs_write_seq(dsi, 0xfc, 0xa5, 0xa5);
+
+	/* Brightness control */
+	/* gamma ca */
+	ea8061v_ams497ee01_update_gamma(ctx, brightness);
+	/* aid b2 */
+	ea8061v_ams497ee01_update_aid(ctx, brightness);
+	/* elvss b6 */
+	ea8061v_ams497ee01_update_elvss(ctx, brightness);
+	/* apply gamma f7 */
+	ea8061v_ams497ee01_gamma_update(ctx);
+
+	ret = mipi_dsi_dcs_exit_sleep_mode(dsi);
+	if (ret < 0) {
+		dev_err(dev, "Failed to exit sleep mode: %d\n", ret);
+		return ret;
+	}
+	msleep(120);
+
+	/* lock, f1 */
+	dsi_dcs_write_seq(dsi, 0xf1, 0xa5, 0xa5);
+
+	ret = mipi_dsi_dcs_set_display_on(dsi);
+	if (ret < 0) {
+		dev_err(dev, "Failed to set display on: %d\n", ret);
+		return ret;
+	}
+
+	return 0;
+}
+
+static int ea8061v_ams497ee01_power_on(struct ea8061v_ams497ee01 *ctx)
+{
+	int ret;
+
+	ret = regulator_bulk_enable(ARRAY_SIZE(ctx->supplies), ctx->supplies);
+	if (ret < 0) {
+		dev_err(&ctx->dsi->dev, "Failed to enable regulators: %d\n", ret);
+		return ret;
+	}
+
+	/* send reset pulse */
+	gpiod_set_value_cansleep(ctx->reset_gpio, 0);
+	msleep(20);
+	gpiod_set_value_cansleep(ctx->reset_gpio, 1);
+	usleep_range(1000, 2000);
+	gpiod_set_value_cansleep(ctx->reset_gpio, 0);
+	msleep(20);
+
+	return 0;
+}
+
+static int ea8061v_ams497ee01_power_off(struct ea8061v_ams497ee01 *ctx)
+{
+	int ret;
+
+	gpiod_set_value(ctx->reset_gpio, 1);
+
+	ret = regulator_bulk_disable(ARRAY_SIZE(ctx->supplies), ctx->supplies);
+	if (ret < 0)
+		return ret;
+
+	return 0;
+}
+
+static int ea8061v_ams497ee01_enable(struct drm_panel *panel)
+{
+	struct ea8061v_ams497ee01 *ctx = to_ea8061v_ams497ee01(panel);
+	struct mipi_dsi_device *dsi = ctx->dsi;
+	int ret;
+
+	if (ctx->prepared)
+		return 0;
+
+	ret = mipi_dsi_dcs_exit_sleep_mode(dsi);
+	if (ret < 0) {
+		dev_err(&dsi->dev, "Failed to exit sleep mode: %d\n", ret);
+		return ret;
+	};
+	msleep(120);
+
+	ret = mipi_dsi_dcs_set_display_on(dsi);
+	if (ret < 0) {
+		dev_err(&dsi->dev, "Failed to set display on: %d\n", ret);
+		return ret;
+	}
+	msleep(50);
+
+	backlight_enable(ctx->bl_dev);
+
+	ctx->enabled = true;
+	return 0;
+}
+
+
+static int ea8061v_ams497ee01_disable(struct drm_panel *panel)
+{
+	struct ea8061v_ams497ee01 *ctx = to_ea8061v_ams497ee01(panel);
+	struct mipi_dsi_device *dsi = ctx->dsi;
+	int ret;
+
+	if (!ctx->enabled)
+		return 0;
+
+	backlight_disable(ctx->bl_dev);
+
 	dsi->mode_flags &= ~MIPI_DSI_MODE_LPM;
 
 	ret = mipi_dsi_dcs_set_display_off(dsi);
 	if (ret < 0) {
-		dev_err(dev, "Failed to set display off: %d\n", ret);
+		dev_err(&dsi->dev, "Failed to set display off: %d\n", ret);
 		return ret;
 	}
 	msleep(35);
 
 	ret = mipi_dsi_dcs_enter_sleep_mode(dsi);
 	if (ret < 0) {
-		dev_err(dev, "Failed to enter sleep mode: %d\n", ret);
+		dev_err(&dsi->dev, "Failed to enter sleep mode: %d\n", ret);
 		return ret;
 	}
 	msleep(100);
 
+	ctx->enabled = false;
 	return 0;
 }
 
@@ -615,19 +671,16 @@ static int ea8061v_ams497ee01_prepare(struct drm_panel *panel)
 	if (ctx->prepared)
 		return 0;
 
-	ret = regulator_bulk_enable(ARRAY_SIZE(ctx->supplies), ctx->supplies);
+	ret = ea8061v_ams497ee01_power_on(ctx);
 	if (ret < 0) {
-		dev_err(dev, "Failed to enable regulators: %d\n", ret);
+		dev_err(dev, "prepare: failed to power on the panel!\n");
 		return ret;
 	}
 
-	ea8061v_ams497ee01_reset(ctx);
-
-	ret = ea8061v_ams497ee01_on(ctx);
+	ret = ea8061v_ams497ee01_init(ctx);
 	if (ret < 0) {
 		dev_err(dev, "Failed to initialize panel: %d\n", ret);
-		gpiod_set_value_cansleep(ctx->reset_gpio, 1);
-		regulator_bulk_disable(ARRAY_SIZE(ctx->supplies), ctx->supplies);
+		ea8061v_ams497ee01_power_off(ctx);
 		return ret;
 	}
 
@@ -644,12 +697,9 @@ static int ea8061v_ams497ee01_unprepare(struct drm_panel *panel)
 	if (!ctx->prepared)
 		return 0;
 
-	ret = ea8061v_ams497ee01_off(ctx);
+	ret = ea8061v_ams497ee01_power_off(ctx);
 	if (ret < 0)
 		dev_err(dev, "Failed to un-initialize panel: %d\n", ret);
-
-	gpiod_set_value_cansleep(ctx->reset_gpio, 1);
-	regulator_bulk_disable(ARRAY_SIZE(ctx->supplies), ctx->supplies);
 
 	ctx->prepared = false;
 	return 0;
@@ -690,6 +740,8 @@ static int ea8061v_ams497ee01_get_modes(struct drm_panel *panel,
 
 static const struct drm_panel_funcs ea8061v_ams497ee01_panel_funcs = {
 	.prepare = ea8061v_ams497ee01_prepare,
+	.enable = ea8061v_ams497ee01_enable,
+	.disable = ea8061v_ams497ee01_disable,
 	.unprepare = ea8061v_ams497ee01_unprepare,
 	.get_modes = ea8061v_ams497ee01_get_modes,
 };
@@ -761,7 +813,6 @@ static int ea8061v_ams497ee01_remove(struct mipi_dsi_device *dsi)
 		dev_err(&dsi->dev, "Failed to detach from DSI host: %d\n", ret);
 
 	drm_panel_remove(&ctx->panel);
-	backlight_device_unregister(ctx->bl_dev);
 
 	return 0;
 }
